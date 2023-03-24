d908b2d0f3746b6002e4c917b1bf91695ebfab9f
sbiljeek <sbiljeekdev@gmail.com>
2023-03-24 16:22:43 +0300

    Initial Commit

---
+++ SwiftUIAppStoreRatingsDistributionChart/SwiftUIAppStoreRatingsDistributionChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppStoreRatingsDistributionChartApp.swift
+//  SwiftUIAppStoreRatingsDistributionChart
+//
+//  Created by Salman Biljeek on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppStoreRatingsDistributionChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
